b656bff2c192201e086a8434b486d9a645bd00ac
Ariel Vieira <arielvieira@users.noreply.github.com>
2023-03-15 00:48:45 -0300

    initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,10 @@
+// uncommenting this will make wrangler throw the error ReferenceError: process is not defined
+// import 'twin.macro'
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Welcome to Remix with twin.macro</h1>
+    </div>
+  );
+}
